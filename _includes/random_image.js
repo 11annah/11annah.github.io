08aa89d8b56195d337352b4ad12c8ec4d82c643e
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-02-15 23:09:20 +0100

    Update random_image.js

--- _includes/random_image.js
+++ _includes/random_image.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var button = document.getElementById("randomButton");
     var image = document.getElementById("randomImage");
-  
+
     button.addEventListener("click", function() {
-      fetch("/random_image.json")
-        .then(response => response.json())
-        .then(data => {
-          var randomImage = data.random_image;
-          image.src = randomImage;
-        })
-        .catch(error => console.error('Error fetching random image:', error));
+        fetch("/random_image.json")
+            .then(response => response.json())
+            .then(data => {
+                var randomImage = data.random_image;
+                image.src = randomImage;
+            })
+            .catch(error => console.error('Error fetching random image:', error));
     });
-  });
+});
+
   
